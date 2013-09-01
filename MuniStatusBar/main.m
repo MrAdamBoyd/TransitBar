18bae46644d2cb8893872a6b7696c06801aa5955
adviceadam <adam@Adams-MacBook-Pro-2.local>
2013-09-01 13:58:54 -0700

    Xcode Project

---
+++ MuniStatusBar/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MuniStatusBar
+//
+//  Created by Adam on 8/31/13.
+//  Copyright (c) 2013 AdamBoyd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
