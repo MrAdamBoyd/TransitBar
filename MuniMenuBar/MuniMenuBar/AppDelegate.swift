074aa172750fc23b173e0d39836455b60368bc30
Adam Boyd <adamboyd50@gmail.com>
2015-08-18 11:41:13 -0700

    new xcode project

---
+++ MuniMenuBar/MuniMenuBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MuniMenuBar
+//
+//  Created by Adam on 2015-08-18.
+//  Copyright (c) 2015 Adam Boyd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
