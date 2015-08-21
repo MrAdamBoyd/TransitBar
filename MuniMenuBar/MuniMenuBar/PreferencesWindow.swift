e4ea75028d3c3687855ebce28d6ce391c3db9e40
Adam Boyd <adam.boyd@cir.ca>
2015-08-21 18:11:53 -0400

    ui for preferences window

--- MuniMenuBar/MuniMenuBar/PreferencesWindow.swift
+++ MuniMenuBar/MuniMenuBar/PreferencesWindow.swift
@@ -11,6 +11,28 @@ import Cocoa
 
 class PreferencesWindow:NSWindow {
     
+    //Optionally show different lines at different times
+    @IBOutlet weak var differentLinesCheckmark: NSButton!
+    @IBOutlet weak var startTimePopup: NSPopUpButton!
+    @IBOutlet weak var endTimePopup: NSPopUpButton!
+    
+    //First line
+    @IBOutlet weak var line1: NSPopUpButton!
+    @IBOutlet weak var stop1: NSPopUpButton!
+    
+    //Second line
+    @IBOutlet weak var line2: NSPopUpButton!
+    @IBOutlet weak var stop2: NSPopUpButton!
+    
+    //Third line, first optional line
+    @IBOutlet weak var line3: NSPopUpButton!
+    @IBOutlet weak var stop3: NSPopUpButton!
+    
+    //Fourth line, second optional line
+    @IBOutlet weak var line4: NSPopUpButton!
+    @IBOutlet weak var stop4: NSPopUpButton!
+    
+    
     override init(contentRect: NSRect, styleMask aStyle: Int, backing bufferingType: NSBackingStoreType, defer flag: Bool) {
         
         super.init(contentRect: contentRect, styleMask: aStyle, backing: bufferingType, defer: flag)
