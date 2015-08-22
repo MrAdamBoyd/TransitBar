22fc9c8dd4b925b99cdd165434fce860ca2b8a91
Adam Boyd <adam.boyd@cir.ca>
2015-08-21 20:53:23 -0400

    enabling and disabling the direction pickers

--- MuniMenuBar/MuniMenuBar/PreferencesWindow.swift
+++ MuniMenuBar/MuniMenuBar/PreferencesWindow.swift
@@ -18,18 +18,22 @@ class PreferencesWindow:NSWindow, MMBXmlParserDelegate, NSTextFieldDelegate {
     
     //First line
     @IBOutlet weak var line1: NSPopUpButton!
+    @IBOutlet weak var direction1: NSPopUpButton!
     @IBOutlet weak var stop1: NSPopUpButton!
     
     //Second line
     @IBOutlet weak var line2: NSPopUpButton!
+    @IBOutlet weak var direction2: NSPopUpButton!
     @IBOutlet weak var stop2: NSPopUpButton!
     
     //Third line, first optional line
     @IBOutlet weak var line3: NSPopUpButton!
+    @IBOutlet weak var direction3: NSPopUpButton!
     @IBOutlet weak var stop3: NSPopUpButton!
     
     //Fourth line, second optional line
     @IBOutlet weak var line4: NSPopUpButton!
+    @IBOutlet weak var direction4: NSPopUpButton!
     @IBOutlet weak var stop4: NSPopUpButton!
     
     
@@ -65,6 +69,25 @@ class PreferencesWindow:NSWindow, MMBXmlParserDelegate, NSTextFieldDelegate {
         endDatePicker.enabled = enabledOrDisabled
     }
     
+    //One of the line popups was selected
+    @IBAction func lineSelected(sender: AnyObject) {
+        //TODO:
+        //Start downloading data for that line
+        if let popup = sender as? NSPopUpButton {
+            /*The direction is enabled by the index of the selected item being a bool
+            The 0th item is "--", so all other values should result in it being enabled
+            0 means false and all other selections will be true */
+            if popup == line1 {
+                direction1.enabled = Bool(popup.indexOfSelectedItem)
+            } else if popup == line2 {
+                direction2.enabled = Bool(popup.indexOfSelectedItem)
+            } else if popup == line3 {
+                direction3.enabled = Bool(popup.indexOfSelectedItem)
+            } else if popup == line4 {
+                direction4.enabled = Bool(popup.indexOfSelectedItem)
+            }
+        }
+    }
     //MARK: MMBXmlParserProtocol
     
     func allLinesDataFinishedLoading() {
