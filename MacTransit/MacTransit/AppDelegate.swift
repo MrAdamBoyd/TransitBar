a46648ab8ab796587c538ab57e240560a04145f8
Adam Boyd <adamboyd50@gmail.com>
2015-12-25 17:44:46 -0800

    branching off to mactransit, going to be a complete rewrite

---
+++ MacTransit/MacTransit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacTransit
+//
+//  Created by Adam Boyd on 2015-12-25.
+//  Copyright © 2015 Adam Boyd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
