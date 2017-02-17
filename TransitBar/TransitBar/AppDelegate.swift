ca0b45900aac84798eddd923d53c9b20e259eb01
Adam Boyd <adamboyd50@gmail.com>
2017-02-17 09:30:28 -0800

    getting distance working properly

--- TransitBar/TransitBar/AppDelegate.swift
+++ TransitBar/TransitBar/AppDelegate.swift
@@ -151,7 +151,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
         
         self.statusItem.menu?.removeAllItems()
         
-        for entry in DataController.shared.savedEntries {
+        for (index, entry) in DataController.shared.savedEntries.enumerated() {
             //When clicking on the menu, all the stops always show
             let title = "\(entry.stop.routeTitle) -> \(entry.stop.direction)"
             
@@ -159,25 +159,9 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
             
             if DataController.shared.displayWalkingTime {
                 
-                //If walking time should be displayed
-                if let location = self.currentLocation {
-                    
-                    //Get the actual distance to the location
-                    let stopLocation = CLLocation(latitude: entry.stop.lat, longitude: entry.stop.lon)
-                    let distance = abs(location.distance(from: stopLocation))
-                    
-                    //Format the string
-                    let df = MKDistanceFormatter()
-                    df.unitStyle = .full
-                    
-                    self.statusItem.menu?.addItem(NSMenuItem(title: "Distance: \(df.string(fromDistance: distance))", action: nil, keyEquivalent: ""))
-                    
-                } else {
-                    
-                    //Unknown distance
-                    self.statusItem.menu?.addItem(NSMenuItem(title: "Distance: unknown", action: nil, keyEquivalent: ""))
-                    
-                }
+                self.statusItem.menu?.addItem(NSMenuItem(title: self.locationTextFrom(source: self.currentLocation, to: CLLocation(latitude: entry.stop.lat, longitude: entry.stop.lon)), action: nil, keyEquivalent: ""))
+                self.setWalkingTimeForMenuItemWith(entry: entry, at: index) //Async gets the walking time
+                
                 self.statusItem.menu?.addItem(NSMenuItem.separator())
             }
             
@@ -249,11 +233,149 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
         }
     }
     
+    /// The index of the menu item for the entry index (2nd entry would be the 6th menu item)
+    ///
+    /// - Parameter index: index of the entry
+    /// - Returns: index in the menu
     func menuItemIndexForEntryIndex(_ index: Int) -> Int {
         if DataController.shared.displayWalkingTime {
-            return index / 3
+            return index * 3
         } else {
             return index
+        }
+    }
+    
+    /// Builds the string for the menu item that contains the distance and walking time to that stop
+    ///
+    /// - Parameters:
+    ///   - source: user's location
+    ///   - destination: stop's location
+    ///   - overrideDistance: use this distance instead of calculating
+    ///   - walkingTime: include the walking time to format this
+    /// - Returns: formatted string
+    func locationTextFrom(source: CLLocation?, to destination: CLLocation?, overrideDistance: CLLocationDistance? = nil, walkingTime: TimeInterval? = nil) -> String {
+        
+        var returnString = ""
+        
+        if let distance = overrideDistance {
+            //Use this distance instead of calculating
+            
+            returnString = "Distance: \(self.formatDistance(distance))"
+            
+        } else if let location = source, let destinationLocation = destination {
+            
+            //Get the actual distance to the location
+            let distance = location.distance(from: destinationLocation)
+            
+            returnString = "Distance: \(self.formatDistance(distance))"
+            
+        } else {
+            
+            //Unknown distance
+            returnString = "Distance: unknown"
+        }
+        
+        if let walkingTime = walkingTime {
+            let toMinutes = Int(round((walkingTime / 60).truncatingRemainder(dividingBy: 60)))
+            returnString.append("; walking time: \(toMinutes) minutes")
+        }
+        
+        return returnString
+    }
+    
+    
+    /// Formats distance in the locality that user has set
+    ///
+    /// - Parameter distance: distance to format
+    /// - Returns: formatted string
+    func formatDistance(_ distance: CLLocationDistance) -> String {
+        //Format the string
+        let df = MKDistanceFormatter()
+        df.unitStyle = .full
+        
+        return df.string(fromDistance: abs(distance))
+    }
+    
+    // MARK: - Dealing with locations
+    
+    /// Gets the walking time for the user's current location to the provided entry. Updates menu item when done
+    ///
+    /// - Parameters:
+    ///   - entry: entry to calculate distance ot
+    ///   - index: entry index of the item
+    func setWalkingTimeForMenuItemWith(entry: TransitEntry, at index: Int) {
+        self.directionsRequestFrom(source: self.currentLocation, destination: CLLocation(latitude: entry.stop.lat, longitude: entry.stop.lon)) { directionsRequest in
+            
+            if let directionsRequest = directionsRequest {
+                
+                let directions = MKDirections(request: directionsRequest)
+                directions.calculate() { [unowned self] response, error in
+                    
+                    if let routes = response?.routes {
+                        let quickest = routes.sorted() {$0.expectedTravelTime < $1.expectedTravelTime}[0]
+                        self.statusItem.menu?.items[self.menuItemIndexForEntryIndex(index) + 1].title = self.locationTextFrom(source: nil, to: nil, overrideDistance: quickest.distance, walkingTime: quickest.expectedTravelTime)
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+    
+    /// Builds an MKDirectionsRequest from the provided source and destination locations
+    ///
+    /// - Parameters:
+    ///   - source: where the user currently is
+    ///   - destination: location of the transit stop
+    ///   - completion: contains the finished mkdirectionsrequest
+    func directionsRequestFrom(source: CLLocation?, destination: CLLocation?, completion: @escaping (MKDirectionsRequest?) -> Void) {
+        guard let sourceLocation = source, let destinationLocation = destination else {
+            //Only works if both items have a location
+            completion(nil)
+            return
+        }
+        
+        var sourceMapItem: MKMapItem?
+        var destMapItem: MKMapItem?
+        
+        //Using a dispatch group for organizing the async work
+        let group = DispatchGroup()
+        group.enter() //For source
+        group.enter() //For destination
+        
+        let geocoder1 = CLGeocoder()
+        geocoder1.reverseGeocodeLocation(sourceLocation) { [unowned self] placemarks, error in
+            sourceMapItem = self.mkmapItemFrom(placemarks: placemarks)
+            group.leave()
+        }
+        
+        let geocoder2 = CLGeocoder()
+        geocoder2.reverseGeocodeLocation(destinationLocation) { [unowned self] placemarks, error in
+            destMapItem = self.mkmapItemFrom(placemarks: placemarks)
+            group.leave()
+        }
+        
+        group.notify(queue: .main) {
+            //Work is now done
+            let request = MKDirectionsRequest()
+            request.source = sourceMapItem
+            request.destination = destMapItem
+            request.requestsAlternateRoutes = true
+            request.transportType = .walking
+            
+            completion(request)
+        }
+    }
+    
+    /// builds an MKMapItem from the provided array of placemarks
+    ///
+    /// - Parameter placemarks: optional array of placemarks
+    /// - Returns: optional map item
+    func mkmapItemFrom(placemarks: [CLPlacemark]?) -> MKMapItem? {
+        if let placemark = placemarks?.first {
+            return MKMapItem(placemark: MKPlacemark(coordinate: placemark.location!.coordinate, addressDictionary: placemark.addressDictionary as! [String:AnyObject]?))
+        } else {
+            return nil
         }
     }
     
